dfae6b678ce2dfc737f68bd51d47cc57060bb143
maren428 <caureola@gmail.com>
2026-02-20 17:45:41 +0100

    aggiunto esercizio_10 a esercizi2

---
+++ 03_Day_Booleans_operators_date/my_solutions/esercizi2/esercizio_10.js
@@ -0,0 +1,10 @@
+let mioNome="Giovanna";
+if (mioNome.length>=7){
+    console.log("Il tuo nome è lungo")
+}else{
+    console.log("Il tuo nome è corto");
+}
+
+//con ternary operator
+let nome="Lucia";
+console.log(nome.length>7? "Il tuo nome è lungo" : "Il tuo nome è corto");
