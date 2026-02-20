d70ac4926e4fb612230fc993544fc9d0694b493e
maren428 <caureola@gmail.com>
2026-02-20 17:36:02 +0100

    aggiunto esercizi_2 a esercizi2

---
+++ 03_Day_Booleans_operators_date/my_solutions/esercizi2/esercizio_2.js
@@ -0,0 +1,7 @@
+let lato_a= prompt("Inserisci il lato a");
+let lato_b= prompt("Inserisci il lato b");
+let lato_c= prompt("Inserisci il lato c");
+lato_a= Number(lato_a);
+lato_b= Number(lato_b);
+lato_c= Number(lato_c);
+alert(`Il perimetro del triangolo è: ${lato_a + lato_b + lato_c}`);
