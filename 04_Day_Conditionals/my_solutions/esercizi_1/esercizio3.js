ae21a08adcca431c077b0beef4b853bb4d56158b
maren428 <caureola@gmail.com>
2026-02-20 17:55:17 +0100

    aggiunto esercizio3 a esercizi_1

---
+++ 04_Day_Conditionals/my_solutions/esercizi_1/esercizio3.js
@@ -0,0 +1,9 @@
+let a=5;
+let b=15;
+if(a>b){
+    console.log(`${a} è piu grande di ${b}`)
+}else{
+    console.log(`${a} è piu piccolo di ${b}`)
+}
+
+(a>b)? console.log(`${a} è piu grande di ${b}`) : console.log(`${a} è piu piccolo di ${b}`);
