b886185686485fab4879c5de3eb83cff5951a299
maren428 <caureola@gmail.com>
2026-02-20 17:44:22 +0100

    aggiunto esercizio_6_7 a esercizi2

---
+++ 03_Day_Booleans_operators_date/my_solutions/esercizi2/esercizio_6_7.js
@@ -0,0 +1,11 @@
+/*m = (y2-y1)/(x2-x1)
+a(2,2) b(6,10) */
+let x1=2;
+let y1=2;
+let x2=6;
+let y2=10;
+let m= (y2-y1)/(x2-x1);
+console.log(m);
+
+let m2=2;
+console.log("I due slope sono uguali?" ,m===m2); //posso aggiungere quante cose voglio in console.log come se fossero dei parametri e li separo con delle ,
