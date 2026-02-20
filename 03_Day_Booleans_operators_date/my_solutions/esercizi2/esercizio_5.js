b406dcccb44d6b2fc17af4f59b4b9fd0851b1403
maren428 <caureola@gmail.com>
2026-02-20 17:37:13 +0100

    aggiunto esercizi_5 a esercizi2

---
+++ 03_Day_Booleans_operators_date/my_solutions/esercizi2/esercizio_5.js
@@ -0,0 +1,14 @@
+//y=2x-2
+let m=2;
+let q=-2;
+let x=5;
+let y1= m*x + q; //in javascript non esistono i coefficienti, ma solo number che vengono moltiplicati ad altri number
+console.log(y1);
+
+let x2=0;
+let y2= m*x2 + q;
+console.log(y2);
+
+let y3=0;
+let x3= -q / m;
+console.log(x3);
