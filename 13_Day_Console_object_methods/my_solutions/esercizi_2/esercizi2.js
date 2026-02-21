eb65d18f59cc4cc41457270b3a21748aac66a507
maren428 <caureola@gmail.com>
2026-02-21 11:11:09 +0100

    aggiunto esercizi2

---
+++ 13_Day_Console_object_methods/my_solutions/esercizi_2/esercizi2.js
@@ -0,0 +1,9 @@
+//#region ESERCIZIO 1
+console.assert(10 > 2*10, "10 non è maggiore di 2*10");
+//#endregion
+//#region ESERCIZIO 2
+console.warn("Attenzione questo è un avviso");
+//#endregion
+//#region ESERCIZIO 3
+console.error("Questo è un messaggio di errore")
+//#endregion
